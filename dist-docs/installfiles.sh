da3ec2c29e18f2d4c7ec7b837061c2167f68472d
Masayuki Nii <nii@msyk.net>
2026-05-10 22:58:55 +0900

    Update versions for package dependencies and add debug directory info logs in install script

--- dist-docs/installfiles.sh
+++ dist-docs/installfiles.sh
@@ -9,23 +9,36 @@ cd "${originalPath}"
 
 # rm -rf vendor node_modules
 
+echo "******************************************************"
+echo "Target Directory: $(pwd)"
+echo "******************************************************"
 mv '__Did_you_run_composer_update.txt' spec/tempfile
 composer update --with-all-dependencies
+npm update
 pnpm install --no-frozen-lockfile
 pnpm install --frozen-lockfile
 mv spec/tempfile '__Did_you_run_composer_update.txt'
 
 cd spec/run
+echo "******************************************************"
+echo "Target Directory: $(pwd)"
+echo "******************************************************"
 npm install --before 2026-03-23
 pnpm install --no-frozen-lockfile
 pnpm install --frozen-lockfile
 
 cd ../run_v8
+echo "******************************************************"
+echo "Target Directory: $(pwd)"
+echo "******************************************************"
 npm install --before 2026-03-23
 pnpm install --no-frozen-lockfile
 pnpm install --frozen-lockfile
 
 cd ../run-safari
+echo "******************************************************"
+echo "Target Directory: $(pwd)"
+echo "******************************************************"
 npm install --before 2026-03-23
 pnpm install --no-frozen-lockfile
 pnpm install --frozen-lockfile
